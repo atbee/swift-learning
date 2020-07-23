063cbb50d4636f378f8b38092830253f7254f4f5
atbee <atb@odds.team>
2020-07-23 16:15:18 +0700

    Learning variable and constant :bee:

---
+++ BasicSyntax.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Variable and Constant
+
+let firstName: String = "Athibet"
+var number: Int = 12
+
+// Type infer
+var implictiDouble = 12.0
+var explicitDouble: Double = 12
