31d8a5f7091ae79335bdf14c3e7ecf9418e2d002
atbee <atb@odds.team>
2020-07-25 21:12:51 +0700

    Learning number :bee:

--- BasicSyntax.playground/Contents.swift
+++ BasicSyntax.playground/Contents.swift
@@ -10,3 +10,18 @@ var number: Int = 12
 // Type infer
 var implictiDouble = 12.0
 var explicitDouble: Double = 12
+
+// Number and String
+
+var numInt: Int = 11
+var numUnsignInt: UInt = 11
+var numFloar: Float = 13.0 // 32 bits
+var numDouble: Double = 13.0 // 64 bits
+
+var money = 1_000_000
+
+// Number casting
+let amount = 12.5
+let amountInt = Int(amount)
+let amountRound = Int(amount.rounded())
+
